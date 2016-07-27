007536c57fb04c13203f10b6b455faadddb13c7e
vvv <vuchkan.vladimir@gmail.com>
2016-07-27 18:16:54 +0300

    hello world state

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+
+
+var config = {
+  entry: path.resolve(__dirname, 'app/index.jsx'),
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/api|uploads*': {
+        target: 'http://192.168.0.200:4000',
+        secure: false
+      }
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(jsx|js)$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'postcss', 'sass']
+      },
+      {
+        test: /\.svg$/,
+        include: [path.resolve(__dirname, 'app', 'images')],
+        exclude: [path.resolve(__dirname, 'app', 'images', 'static')],
+        loader: 'svg-inline'
+      },
+      {
+        test: /\.svg$|\.woff$/,
+        include: [
+          path.resolve(__dirname, 'app', 'images', 'static'),
+          path.resolve(__dirname, 'app', 'fonts')
+        ],
+        loader: 'url',
+        query: {
+          limit: 100000
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  },
+  postcss() {
+    return [autoprefixer({browsers: '> 0%'})];
+  },
+  resolve: {
+    root: [
+      path.resolve('./app')
+    ],
+    extensions: ['', '.js', '.jsx', '.scss']
+  }
+};
+
+module.exports = config;
