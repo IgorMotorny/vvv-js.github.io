f909210979bc821db21d5076c2a1ad6f2e15bb25
vvv <vuchkan.vladimir@gmail.com>
2016-07-31 01:05:02 +0300

    education block

--- app/views/CV/index.jsx
+++ app/views/CV/index.jsx
@@ -10,6 +10,7 @@ import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
 import RaisedButton from 'material-ui/RaisedButton';
 import Description from 'material-ui/svg-icons/action/description';
+import Link from 'material-ui/svg-icons/content/link';
 import FontIcon from 'material-ui/FontIcon';
 import FlatButton from 'material-ui/FlatButton';
 import Facebook from 'images/facebook-logo.svg';
@@ -17,6 +18,9 @@ import Linkedin from 'images/linkedin-logo.svg';
 import Github from 'images/github-logo.svg';
 import LinearProgress from 'material-ui/LinearProgress';
 import Chip from 'material-ui/Chip';
+import {GridList, GridTile} from 'material-ui/GridList';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import Subheader from 'material-ui/Subheader';
 
 import {indigo600, indigo900, grey800} from 'material-ui/styles/colors';
 
@@ -33,16 +37,50 @@ export default class CV extends React.Component {
       wrapper: {
         display: 'flex',
         flexWrap: 'wrap',
+      },
+      root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      },
+      gridList: {
+        width: 670,
+        height: 408,
+        overflowY: 'auto',
+        marginBottom: 0
       }
     };
 
+    this.tilesData = [
+      {
+        img: '/app/images/cert1.png',
+        title: 'AngularJS',
+        author: '916/100',
+        href: 'http://www.testprovider.com/ru/Certificate/Search/TP45751316'
+      },
+      {
+        img: '/app/images/cert2.png',
+        title: 'HTML/CSS',
+        author: '892/1000',
+        href: 'http://www.testprovider.com/ru/Certificate/Search/TP41707053'
+      },
+      {
+        img: '/app/images/cert3.png',
+        title: 'JavaScript Advanced',
+        author: '875/1000',
+        href: 'http://www.testprovider.com/ru/Certificate/Search/TP45395707'
+      },
+      {
+        img: '/app/images/cert4.png',
+        title: 'JavaScript Essential',
+        author: '900/1000',
+        href: 'http://www.testprovider.com/ru/Certificate/Search/TP92360944'
+      }]
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
 
   handleClose = () => this.setState({open: false});
-
-
 
   render() {
     return (
@@ -73,9 +111,9 @@ export default class CV extends React.Component {
               <div className="main-info-contact">
                 <div className="contact-list">
                   <div><span>Age</span> 18</div>
+                  <div><span>Experience</span> 1 year</div>
                   <div><span>Email</span> vuchkan.vladimir@gmail.com</div>
                   <div><span>Phone</span> +38 (066) 0375-901</div>
-                  <div><span>Возраст</span> 18</div>
                 </div>
                 <div className="download-cv"> <RaisedButton
                   label="Download CV"
@@ -156,6 +194,38 @@ export default class CV extends React.Component {
               <Chip style={this.styles.chip} onTouchTap={()=>{}}>Angular</Chip>
               <Chip style={this.styles.chip} onTouchTap={()=>{}}>Scrum</Chip>
               <Chip style={this.styles.chip} onTouchTap={()=>{}}>SQL</Chip>
+            </div>
+          </Paper>
+        </div>
+        <div className="education-wrapper">
+          <Paper style={{backgroundColor: '#f1f1f1'}} className="education-list" zDepth={2}>
+            <h1>Education</h1>
+            <h2>University</h2>
+            <div className="university-box">
+              <Avatar className="university-logo" style={{backgroundColor: '#ffffff'}} size={80} src="/app/images/kpi-logo.png" />
+              <div className="university-info-box">
+                <p className="university-name">NTUU 'KPI'</p>
+                <Subheader style={{lineHeight: 'auto', paddingLeft: '0'}}>Faculty of Program engineering, 2014 - present</Subheader>
+              </div>
+            </div>
+            <h2>Certificates</h2>
+            <div style={this.styles.root}>
+              <GridList
+                cellHeight={200}
+                style={this.styles.gridList}
+              >
+                {this.tilesData.map((tile) => (
+                  <GridTile
+                    key={tile.img}
+                    title={<b>{tile.title}</b>}
+                    subtitle={<span>{tile.author}</span>}
+                    titleBackground={'rgba(0, 188, 212, 0.8)'}
+                    actionIcon={<IconButton target="_blank" href={tile.href}><Link color="white" /></IconButton>}
+                  >
+                    <img src={tile.img} />
+                  </GridTile>
+                ))}
+              </GridList>
             </div>
           </Paper>
         </div>
